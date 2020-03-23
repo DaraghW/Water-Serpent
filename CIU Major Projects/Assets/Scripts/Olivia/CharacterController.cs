1a06202cf3059cc615c553ea561a3fcdb2217676
oliviacarbone <49055820+oliviacarbone@users.noreply.github.com>
2020-03-23 22:59:58 +0800

    CONSUMABLES: Cockroach Speed Boost

---
+++ CIU Major Projects/Assets/Scripts/Olivia/CharacterController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+
+    Vector2 movement; 
+
+    void Update()
+    {
+        //update isnt a good place to do physics, instead use fixed update
+        //handling inputs
+
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        //executed on a fixed timer, called 50 times a second
+        //handling physics movemnet
+
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
---
+++ CIU Major Projects/Assets/Scripts/Olivia/Consumables.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Consumables : MonoBehaviour
+{
+    private CharacterController charcontroller;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        charcontroller = FindObjectOfType<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag("Cockroach"))
+        {
+            Debug.Log("IT HIT");
+            Destroy(collider.gameObject);
+            StopCoroutine(SpeedBoost());
+            StartCoroutine(SpeedBoost());
+        }
+    }
+
+    IEnumerator SpeedBoost()
+    {
+        charcontroller.moveSpeed = 10f;
+        yield return new WaitForSeconds(5f);
+        charcontroller.moveSpeed = 5f;
+    }
+}
