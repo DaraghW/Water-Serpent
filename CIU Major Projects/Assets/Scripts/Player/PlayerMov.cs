0033c37943ee2e47e7f7299e81ded9a05affd608
DaraghW <1015125@student.sae.edu.au>
2020-03-30 18:03:23 +0800

    fixing tail

--- CIU Major Projects/Assets/Scripts/Player/PlayerMov.cs
+++ CIU Major Projects/Assets/Scripts/Player/PlayerMov.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class PlayerMov : MonoBehaviour
 {
     public float moveSpeed;
+    public float turnDelay;
+    private bool canTurn;
 
     public int maxLength;
+    private bool canMakeTail;
     public float timer;
     public float maxTimer;
 
@@ -32,35 +35,65 @@ public class PlayerMov : MonoBehaviour
         float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
         */
-
-        if (Input.GetKeyDown(KeyCode.W) && transform.rotation != Quaternion.Euler(0, 0, -90))
+        
+        //WASD controlls
+        turnDelay -= 1 * Time.deltaTime;
+        if (turnDelay <= 0)
+        {
+            canTurn = true;
+        }
+        if (Input.GetKeyDown(KeyCode.W) && transform.rotation != Quaternion.Euler(0, 0, -90) && canTurn == true)
         {
             transform.rotation = Quaternion.Euler(0, 0, 90);
+            canTurn = false;
+            turnDelay = .1f;
         }
 
-        if (Input.GetKeyDown(KeyCode.A) && transform.rotation != Quaternion.Euler(0, 0, 0))
+        if (Input.GetKeyDown(KeyCode.A) && transform.rotation != Quaternion.Euler(0, 0, 0) && canTurn == true)
         {
             transform.rotation = Quaternion.Euler(0, 0, 180);
-            //RotateLeft();
+            canTurn = false;
+            turnDelay = .1f;
         }
 
-        if (Input.GetKeyDown(KeyCode.S) && transform.rotation != Quaternion.Euler(0, 0, 90))
+        if (Input.GetKeyDown(KeyCode.S) && transform.rotation != Quaternion.Euler(0, 0, 90) && canTurn == true)
         {
             transform.rotation = Quaternion.Euler(0, 0, -90);
+            canTurn = false;
+            turnDelay = .1f;
         }
 
-        if (Input.GetKeyDown(KeyCode.D) && transform.rotation != Quaternion.Euler(0, 0, 180))
+        if (Input.GetKeyDown(KeyCode.D) && transform.rotation != Quaternion.Euler(0, 0, 180) && canTurn == true)
         {
             transform.rotation = Quaternion.Euler(0, 0, 0);
-            //RotateRight();
+            canTurn = false;
+            turnDelay = .1f;
         }
 
+        
+        
         timer -= 1 * Time.deltaTime;
-        if (timer <= 0)
+        if (GameObject.FindGameObjectsWithTag("Tail").Length >= maxLength)
         {
-            Instantiate(tailPart, tailStart.position,Quaternion.identity);
-            timer = maxTimer;
+            canMakeTail = true;
+            //Debug.Log("make tail");
         }
+
+        if (timer <= 0 && canMakeTail == true)
+        {
+            Instantiate(tailPart, tailStart.position, Quaternion.identity);
+            timer = maxTimer;
+            Debug.Log(GameObject.FindGameObjectsWithTag("Tail").Length);
+        }
+
+        //if (GameObject.FindGameObjectsWithTag("Tail").Length <= maxLength && timer <= 0)
+        //{
+        //    Instantiate(tailPart, tailStart.position, Quaternion.identity);
+        //    timer = maxTimer;
+        //    Debug.Log(GameObject.FindGameObjectsWithTag("Tail").Length);
+        //}
+
+        Debug.Log(GameObject.FindGameObjectsWithTag("Tail").Length); 
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -68,6 +101,14 @@ public class PlayerMov : MonoBehaviour
         if(collision.gameObject.tag == "Tail")
         {
             Debug.Log("oof");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Cockroach")
+        {
+            maxLength += 1;
         }
     }
 }
