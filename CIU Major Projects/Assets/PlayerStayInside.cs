83b2fe91e74ae46e7bb1acef2f3c05ac6f5218b2
BraydenMiller <48822485+BraydenMiller@users.noreply.github.com>
2020-07-14 13:52:15 +0800

    Multiplayer Camera Setup + UI Res changes
    - Setup the camera for the multiplayer system. It zooms out when the players move away from eachother.
    -Player 2 is clamped to player 1 so if they get too far away from them they will be spawned back at their position.
    -This system works off of a trigger collider so changing the shape will affect the distance they can travel apart.
    -Changed the game to 4:3 in line similarly to older arcade screens

---
+++ CIU Major Projects/Assets/PlayerStayInside.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStayInside : MonoBehaviour
+{
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "PlayerBounds")
+        {
+            transform.position = collision.transform.position;
+        }
+    }
+}
