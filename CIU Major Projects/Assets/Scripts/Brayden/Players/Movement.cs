43f2aa05d7d4e03aa6abf5cc110b222d5e9c0657
oliviacarbone <oliviatcarbone@gmail.com>
2020-09-02 18:29:24 +0800

    Arcade Integration Updates
    - Put some inputs for the arcade into the project

--- CIU Major Projects/Assets/Scripts/Brayden/Players/Movement.cs
+++ CIU Major Projects/Assets/Scripts/Brayden/Players/Movement.cs
@@ -2,8 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum InputType { PC, Arcade }
+
+[System.Serializable]
+public class InputMap
+{
+    [SerializeField] public string playerYAxis;
+    [SerializeField] public string playerXAxis;
+    [SerializeField] public KeyCode button1;
+    [SerializeField] public KeyCode button2;
+    [SerializeField] public KeyCode button3;
+    [SerializeField] public KeyCode button4;
+}
+
+[System.Serializable]
+public class PlayerInput
+{
+    [SerializeField] public Vector2 playerAxis;
+    [SerializeField] public bool button1;
+    [SerializeField] public bool button2;
+    [SerializeField] public bool button3;
+    [SerializeField] public bool button4;
+}
+
 public class Movement : MonoBehaviour
 {
+    public InputType inputType = InputType.PC;
+    public SAE.ArcadeMachine.PlayerColorId playerColorId;
+
+    public InputMap inputMap;
+    public PlayerInput playerInput;
+
     public float moveSpeed, regSpeed, turnDelay, maxTailLength,
         maxWaterLength, timer, maxTimer, rTime, tTime;
     
@@ -28,6 +57,12 @@ public class Movement : MonoBehaviour
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
         currentTail = sideTail;
+
+        //SAE.ArcadeMachine.playerPressedButton += this.PlayerPressedButton;
+        //SAE.ArcadeMachine.playerHeldButton += this.PlayerHeldButton;
+        //SAE.ArcadeMachine.playerReleasedButton += this.PlayerReleasedButton;
+        //SAE.ArcadeMachine.playerJoystickAxisChanged += this.PlayerJoystickAxisChanged;
+        //SAE.ArcadeMachine.configurationFinished += this.ConfigurationFinsihed;
     }
 
     // Update is called once per frame
@@ -40,6 +75,10 @@ public class Movement : MonoBehaviour
             canTurn = true;
         }
 
+        GetInputs();
+        ProcessMovement();
+
+        /*
         if (myObject.tag == "Player1")
         {
             Player1Move();
@@ -59,6 +98,8 @@ public class Movement : MonoBehaviour
         {
             Player4Move();
         }
+
+        */
 
         //MAKE TAIL PARTS
         timer -= 1 * Time.deltaTime;
@@ -97,6 +138,87 @@ public class Movement : MonoBehaviour
         }
     }
 
+    /*
+    [SerializeField] public string playerAxis;
+    [SerializeField] public KeyCode button1;
+    [SerializeField] public KeyCode button2;
+    [SerializeField] public KeyCode button3;
+    [SerializeField] public KeyCode button4;
+{
+    [SerializeField] public Vector2 playerAxis;
+    [SerializeField] public bool button1;
+    [SerializeField] public bool button2;
+    [SerializeField] public bool button3;
+    [SerializeField] public bool button4;
+
+        */
+    public void GetInputs()
+    {
+        if (inputType == InputType.PC)
+        {
+            playerInput.playerAxis.x = Input.GetAxis(inputMap.playerXAxis);
+            playerInput.playerAxis.y = Input.GetAxis(inputMap.playerYAxis);
+            playerInput.button1 = Input.GetKeyDown(inputMap.button1);
+            playerInput.button2 = Input.GetKeyDown(inputMap.button2);
+            playerInput.button3 = Input.GetKeyDown(inputMap.button3);
+            playerInput.button4 = Input.GetKeyDown(inputMap.button4);
+        }
+        else if (inputType == InputType.Arcade)
+        {
+            playerInput.playerAxis = SAE.ArcadeMachine.input.PlayerJoystickAxis(playerColorId);
+            playerInput.button1 = SAE.ArcadeMachine.input.PlayerPressingButton(playerColorId, 1);
+            playerInput.button2 = SAE.ArcadeMachine.input.PlayerPressingButton(playerColorId, 2);
+            playerInput.button3 = SAE.ArcadeMachine.input.PlayerPressingButton(playerColorId, 3);
+            playerInput.button4 = SAE.ArcadeMachine.input.PlayerPressingButton(playerColorId, 4);
+        }
+    }
+
+
+    public void ProcessMovement()
+    {
+        if (!canTurn) { return; }
+
+        // Up
+        if (playerInput.playerAxis.y < 0)
+        {
+            spriteRenderer.sprite = headU;
+            currentTail = topTail;
+            rb.velocity = new Vector2(0, moveSpeed);
+            canTurn = false;
+            turnDelay = .2f;
+        }
+        //Down
+        else if (playerInput.playerAxis.y > 0)
+        {
+            spriteRenderer.sprite = headD;
+            currentTail = topTail;
+            rb.velocity = new Vector2(0, -moveSpeed);
+            canTurn = false;
+            turnDelay = .2f;
+        }
+
+        //Left
+        if (playerInput.playerAxis.x < 0)
+        {
+            spriteRenderer.sprite = headL;
+            currentTail = sideTail;
+            rb.velocity = new Vector2(-moveSpeed, 0);
+            canTurn = false;
+            turnDelay = .2f;
+        }
+        //Right
+        else if (playerInput.playerAxis.x > 0)
+        {
+            spriteRenderer.sprite = headR;
+            currentTail = sideTail;
+            rb.velocity = new Vector2(moveSpeed, 0);
+            canTurn = false;
+            turnDelay = .2f;
+        }
+
+    }
+
+    /*
     void Player1Move()
     {
         //UP
@@ -272,6 +394,10 @@ public class Movement : MonoBehaviour
             turnDelay = .2f;
         }
     }
+    */
+
+
+
 
     void MakeTail()
     {
@@ -325,5 +451,27 @@ public class Movement : MonoBehaviour
     {
         maxWaterLength += 3;
         print("rat eaten");
+    }
+
+    // Event handlers.
+    private void PlayerPressedButton(SAE.ArcadeMachine.PlayerColorId playerId, int buttonId)
+    {
+        Debug.Log("PlayerPressedButton: " + playerId.ToString() + "  " + buttonId);
+    }
+    private void PlayerHeldButton(SAE.ArcadeMachine.PlayerColorId playerId, int buttonId)
+    {
+        Debug.Log("PlayerHeldButton: " + playerId.ToString() + "  " + buttonId);
+    }
+    private void PlayerReleasedButton(SAE.ArcadeMachine.PlayerColorId playerId, int buttonId)
+    {
+        Debug.Log("PlayerReleasedButton: " + playerId.ToString() + "  " + buttonId);
+    }
+    private void PlayerJoystickAxisChanged(SAE.ArcadeMachine.PlayerColorId playerId, Vector2 axis)
+    {
+        Debug.Log("PlayerJoystickAxisChanged: " + playerId.ToString() + "  " + axis);
+    }
+    private void ConfigurationFinsihed()
+    {
+        Debug.Log("ConfigurationFinsihed!");
     }
 }
